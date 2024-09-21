fe351cec1c0236c1980cad0f623013223015d003
nashir <nashirnoor1718@gmail.com>
2024-09-21 19:15:46 +0530

    make pass remove

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -25,10 +25,10 @@ class UserSerializer(serializers.ModelSerializer):
         validated_data['password'] = make_password(validated_data.get('password'))
         return super(UserSerializer, self).create(validated_data)
 
-    def update(self, instance, validated_data):
-        if 'password' in validated_data:
-            validated_data['password'] = make_password(validated_data.get('password'))
-        return super(UserSerializer, self).update(instance, validated_data)
+    # def update(self, instance, validated_data):
+    #     if 'password' in validated_data:
+    #         validated_data['password'] = make_password(validated_data.get('password'))
+    #     return super(UserSerializer, self).update(instance, validated_data)
 
 
 class LoginSerializer(TokenObtainPairSerializer):
